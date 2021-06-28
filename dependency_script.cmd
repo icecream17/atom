7039e1affe6c9be3ef5c793f9c5e8599a6140ebc
icecream17 <icecream17.github@gmail.com>
2021-06-28 11:54:50 -0500

    Make it easier to update the sheet

---
+++ dependency_script.cmd
@@ -0,0 +1,170 @@
+
+
+npm doctor
+echo    Quit if npm doctor says "not ok"
+echo    Quit if npm doctor says "not ok"
+echo    Quit if npm doctor says "not ok"
+PAUSE
+
+cd C:\Users\Steven\Documents\GitHub\clones\icecream17\atom
+npm install
+npm audit
+PAUSE
+
+cd C:\Users\Steven\Documents\GitHub\clones\icecream17\atom\apm
+npm install
+npm audit
+PAUSE
+
+cd C:\Users\Steven\Documents\GitHub\clones\icecream17\atom\packages\about
+npm install
+npm audit
+PAUSE
+
+cd C:\Users\Steven\Documents\GitHub\clones\icecream17\atom\packages\atom-dark-syntax
+npm install
+npm audit
+PAUSE
+
+cd C:\Users\Steven\Documents\GitHub\clones\icecream17\atom\packages\atom-dark-ui
+npm install
+npm audit
+PAUSE
+
+cd C:\Users\Steven\Documents\GitHub\clones\icecream17\atom\packages\atom-light-syntax
+npm install
+npm audit
+PAUSE
+
+cd C:\Users\Steven\Documents\GitHub\clones\icecream17\atom\packages\atom-light-ui
+npm install
+npm audit
+PAUSE
+
+cd C:\Users\Steven\Documents\GitHub\clones\icecream17\atom\packages\autoflow
+npm install
+npm audit
+PAUSE
+
+cd C:\Users\Steven\Documents\GitHub\clones\icecream17\atom\packages\base16-tomorrow-dark-theme
+npm install
+npm audit
+PAUSE
+
+cd C:\Users\Steven\Documents\GitHub\clones\icecream17\atom\packages\base16-tomorrow-light-theme
+npm install
+npm audit
+PAUSE
+
+cd C:\Users\Steven\Documents\GitHub\clones\icecream17\atom\packages\dalek
+npm install
+npm audit
+PAUSE
+
+cd C:\Users\Steven\Documents\GitHub\clones\icecream17\atom\packages\deprecation-cop
+npm install
+npm audit
+PAUSE
+
+cd C:\Users\Steven\Documents\GitHub\clones\icecream17\atom\packages\dev-live-reload
+npm install
+npm audit
+PAUSE
+
+cd C:\Users\Steven\Documents\GitHub\clones\icecream17\atom\packages\exception-reporting
+npm install
+npm audit
+PAUSE
+
+cd C:\Users\Steven\Documents\GitHub\clones\icecream17\atom\packages\git-diff
+npm install
+npm audit
+PAUSE
+
+cd C:\Users\Steven\Documents\GitHub\clones\icecream17\atom\packages\go-to-line
+npm install
+npm audit
+PAUSE
+
+cd C:\Users\Steven\Documents\GitHub\clones\icecream17\atom\packages\grammar-selector
+npm install
+npm audit
+PAUSE
+
+cd C:\Users\Steven\Documents\GitHub\clones\icecream17\atom\packages\incompatible-packages
+npm install
+npm audit
+PAUSE
+
+cd C:\Users\Steven\Documents\GitHub\clones\icecream17\atom\packages\language-rust-bundled
+npm install
+npm audit
+PAUSE
+
+cd C:\Users\Steven\Documents\GitHub\clones\icecream17\atom\packages\line-ending-selector
+npm install
+npm audit
+PAUSE
+
+cd C:\Users\Steven\Documents\GitHub\clones\icecream17\atom\packages\link
+npm install
+npm audit
+PAUSE
+
+cd C:\Users\Steven\Documents\GitHub\clones\icecream17\atom\packages\one-dark-syntax
+npm install
+npm audit
+PAUSE
+
+cd C:\Users\Steven\Documents\GitHub\clones\icecream17\atom\packages\one-dark-ui
+npm install
+npm audit
+PAUSE
+
+cd C:\Users\Steven\Documents\GitHub\clones\icecream17\atom\packages\one-light-syntax
+npm install
+npm audit
+PAUSE
+
+cd C:\Users\Steven\Documents\GitHub\clones\icecream17\atom\packages\one-light-ui
+npm install
+npm audit
+PAUSE
+
+cd C:\Users\Steven\Documents\GitHub\clones\icecream17\atom\packages\solarized-dark-syntax
+npm install
+npm audit
+PAUSE
+
+cd C:\Users\Steven\Documents\GitHub\clones\icecream17\atom\packages\solarized-light-syntax
+npm install
+npm audit
+PAUSE
+
+cd C:\Users\Steven\Documents\GitHub\clones\icecream17\atom\packages\update-package-dependencies
+npm install
+npm audit
+PAUSE
+
+cd C:\Users\Steven\Documents\GitHub\clones\icecream17\atom\packages\welcome
+npm install
+npm audit
+PAUSE
+
+cd C:\Users\Steven\Documents\GitHub\clones\icecream17\atom\script
+npm install
+npm audit
+PAUSE
+
+cd C:\Users\Steven\Documents\GitHub\clones\icecream17\atom\script\update-server
+npm install
+npm audit
+PAUSE
+
+cd C:\Users\Steven\Documents\GitHub\clones\icecream17\atom\script\vsts
+npm install
+npm audit
+PAUSE
+
+
+
